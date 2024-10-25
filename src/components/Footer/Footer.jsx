1865b0d10694393c1cbf6c5deb189769a98d5a3b
RahniDev <rahni32@protonmail.com>
2024-10-25 17:33:02 +0200

    Add logo, styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,9 +3,10 @@ import styles from './Footer.module.css'
 const Footer = () => {
   return (
     <footer>
-      Twitter 
-      LinkedIn 
-      Facebook
+      <a href="">Twitter</a>
+      <a href="https://www.linkedin.com/company/contryv/">LinkedIn</a> 
+      <a href="https://www.facebook.com/profile.php?id=61557238989038">
+      Facebook</a>
     </footer>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,9 @@
 import styles from './Navbar.module.css'
-
+import logo from '../../assets/logo.webp'
 const Navbar = () => {
   return (
     <nav className={styles.nav}>
-      <a href="#home">Contryv</a>     
+      <a href="#home"><img src={logo} width={110}  alt="logo" /></a>     
       <ul className={styles.navlinks}>
         <li><a href="#services">Services</a></li>
         <li><a href="#projects">Projects</a></li>
